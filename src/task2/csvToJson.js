599a8c79b69430e30a739e4c2b41c7ad215ba521
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-05-09 17:52:53 +0300

    fix: update folder naming for csv file

--- src/task2/csvToJson.js
+++ src/task2/csvToJson.js
@@ -3,12 +3,15 @@ const path = require('path');
 const os = require('os');
 const csv = require('csvtojson');
 
-const filePath = '../../assets/task1.2/';
+const filePath = '../../assets/task1.2/csv/';
 const csvFile = path.join(__dirname, filePath, 'nodejs-hw1-ex1.csv');
 const destinationFile = path.join(__dirname, filePath, 'output.txt');
 
 const readableStream = fs.createReadStream(csvFile);
 const writableStream = fs.createWriteStream(destinationFile);
+
+readableStream.on('error', (error) => console.error('Failed to load file due to error', error));
+writableStream.on('error', (error) => console.error('Failed to write into file due to error', error));
 
 const onError = (error) => {
   console.log(error);
