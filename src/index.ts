b6922bd9188831f19128dbb98954ad96a07ade10
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-05-10 19:55:21 +0300

    feat: add dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import * as dotenv from 'dotenv';
 import App from './app';
 import { PersonService } from './service';
 import { PersonRouter } from './router';
 import { internalErrorMidleware, defaultErrorMiddleware } from './middleware';
+
+dotenv.config();
+const portString = process.env.PORT ? process.env.PORT : '0';
+const port: number = Number.parseInt(portString);
 
 const personService = new PersonService();
 const personRouter = new PersonRouter(personService, '/users');
 const errorHandlers = [internalErrorMidleware, defaultErrorMiddleware];
 
 const app = new App([], [personRouter], errorHandlers);
-app.start(3000);
+console.log(port);
+app.start(port);
