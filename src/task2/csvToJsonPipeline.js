9dad02b1975298ce72eb95cfad7aca555b7bd237
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-05-13 23:02:10 +0300

    feat: update task2 with split library. update pipes to use split and process file line by line

--- src/task2/csvToJsonPipeline.js
+++ src/task2/csvToJsonPipeline.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import csv from 'csvtojson/v2';
-import { Writable } from 'stream';
+import split from 'split';
+import os from 'os';
 
 const rootFolder = path.resolve('.');
 const filePath = './assets/task1.2/csv/';
@@ -10,7 +11,7 @@ const csvFile = path.join(rootFolder, filePath, 'nodejs-hw1-ex1.csv');
 const destinationFile = path.join(rootFolder, filePath, 'output.txt');
 
 const convertCsvToJson = () => {
-  const readableStream = fs.createReadStream(csvFile, { highWaterMark: 64 });
+  const readableStream = fs.createReadStream(csvFile);
   const writableStream = fs.createWriteStream(destinationFile);
 
   readableStream.on('error', (error) => {
@@ -21,24 +22,17 @@ const convertCsvToJson = () => {
   });
   readableStream.on('close', () => writableStream.end());
 
-  const customWriterWithTimeout = new Writable({
-    write(chunk, encoding, callback) {
-      console.log('write chunk', chunk.toString());
-
-      writableStream.write(chunk, encoding);
-      callback();
-    },
-  });
-
   readableStream
+    .pipe(split(new RegExp(`(${os.EOL})`)))
     .pipe(
       csv({
+        noheader: false,
         headers: ['Book', 'Author', 'Amount', 'Price'],
         ignoreColumns: /(Amount)/,
         alwaysSplitAtEOL: true,
-      }).on('data', (data) => console.log('csvToJson', data.toString())),
+      }),
     )
-    .pipe(customWriterWithTimeout);
+    .pipe(writableStream);
 };
 
 export default convertCsvToJson;
