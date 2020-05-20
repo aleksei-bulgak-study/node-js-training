751eb559968670af0727944c7e95da5d27deb31c
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-05-20 22:52:19 +0300

    feat: add winston logger instead of console.log output

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,10 @@
 import * as dotenv from 'dotenv';
 import { databaseConfig, dbUrl } from './database';
 import port from '../configs/port';
+import { loggerService } from './logger';
 
 dotenv.config();
 const portNumber = port();
-const sequelize = databaseConfig(dbUrl());
+const sequelize = databaseConfig(dbUrl(), loggerService);
 
-export { portNumber as port, sequelize };
+export { portNumber as port, sequelize, loggerService };
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,13 @@
 import internalErrorMidleware from './internalError.midleware';
 import defaultErrorMiddleware from './defaultError.middleware';
 import notFoundMiddleware from './notFound.middleware';
+import errorLogMiddleware from './errorLog.middleware';
+import requestLogMiddleware from './requestLog.middleware';
 
-export { internalErrorMidleware, defaultErrorMiddleware, notFoundMiddleware };
+export {
+  internalErrorMidleware,
+  defaultErrorMiddleware,
+  errorLogMiddleware,
+  requestLogMiddleware,
+  notFoundMiddleware,
+};
