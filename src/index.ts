6d358182aac5da0922d31530b43d02b2239bd8ea
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-05-26 18:18:47 +0300

    fix: update router and removed app class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import app from './app';
+import express, { ErrorRequestHandler, Router } from 'express';
 import { PersonService } from './service';
 import { PersonRouter } from './router/person.router';
 import { internalErrorMidleware, defaultErrorMiddleware, notFoundMiddleware } from './middleware';
 import { port } from './config';
 
 const personService = new PersonService();
-const personRouter = PersonRouter(personService, '/users');
+const personRouter = PersonRouter(personService);
 const errorHandlers = [internalErrorMidleware, defaultErrorMiddleware];
 
-app([], [personRouter, notFoundMiddleware], errorHandlers)(port);
+const app = express();
+app.use(express.json());
+app.use('/users', personRouter);
+app.use(notFoundMiddleware);
+app.use(...errorHandlers);
+app.listen(port);
--- src/router/person.router.ts
+++ src/router/person.router.ts
@@ -4,36 +4,32 @@ import { PersonService } from '../service';
 import { fullPersonSchema, createPersonSchema } from '../model';
 import { validatePerson } from '../middleware/personValidation.middleware';
 
-const urlBuilder = (basePath: string, specificPath: string): string => {
-  return basePath + specificPath;
-};
-
-export const PersonRouter = (service: PersonService, path: string): Router => {
+export const PersonRouter = (service: PersonService): Router => {
   const router = Router();
 
-  router.get(urlBuilder(path, '/:id'), (req, res) => {
+  router.get('/:id', (req, res) => {
     const result = service.getById(req.params.id);
     res.status(200).json(result);
   });
 
-  router.post(urlBuilder(path, '/'), validatePerson(createPersonSchema), (req, res) => {
+  router.post('/', validatePerson(createPersonSchema), (req, res) => {
     const result = service.create(req.body);
     res.status(201).json(result);
   });
 
-  router.put(urlBuilder(path, '/:id'), validatePerson(fullPersonSchema), (req, res) => {
+  router.put('/:id', validatePerson(fullPersonSchema), (req, res) => {
     const person = req.body;
     person.id = req.params.id;
     const result = service.update(person);
     res.status(200).json(result);
   });
 
-  router.delete(urlBuilder(path, '/:id'), (req, res) => {
+  router.delete('/:id', (req, res) => {
     const result = service.delete(req.params.id);
     res.status(200).json(result);
   });
 
-  router.get(urlBuilder(path, '/'), (req, res) => {
+  router.get('/', (req, res) => {
     const { loginSubstring = '', limit } = req.query;
     const loginArgument = loginSubstring.toString();
     const result = service.getAutoSuggestUsers(loginArgument, +limit);
